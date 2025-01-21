1e8c864336352a50741678fbab1e6450ace28bdd
jacobhsu <jacob.hsu.tw@gmail.com>
2025-01-21 17:19:43 +0800

    chore: bin/www

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-const port = 3000;
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const swaggerUi = require("swagger-ui-express");
@@ -42,10 +42,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app;
